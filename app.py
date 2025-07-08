fdb9beccfca4dd69d079d1be6cf7e31ebc84e8ca
xAgree <contact.ethanyoung@gmail.com>
2025-07-08 12:27:48 +1200

    Update app.py

--- app.py
+++ app.py
@@ -138,8 +138,8 @@ if uploaded_schedule and uploaded_pax_db:
         except Exception as e:
             st.warning(f"Could not load Domestic: {e}")
 
-    st.write("Departure DataFrame Preview", df_Departure.head())
-    st.write("Departure Rows count:", len(df_Departure))
+    st.write("Departure DataFrame Preview", D.head())
+    st.write("Departure Rows count:", len(D))
     
     # Combine
     df = pd.DataFrame({
